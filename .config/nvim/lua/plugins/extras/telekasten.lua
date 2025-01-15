3855ceaee3bb8c224dd5339c866d50b5db239f3c
Ruslan <admin@ruslan.beer>
2025-01-16 00:07:52 +0300

    delete nvim, tmux configs

--- .config/nvim/lua/plugins/extras/telekasten.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "renerocksai/telekasten.nvim",
-  dependencies = {
-    "nvim-telescope/telescope.nvim",
-    "nvim-telescope/telescope-media-files.nvim",
-    "renerocksai/calendar-vim",
-  },
-}
